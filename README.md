f678f4d9ffd92acfeb909ac0d755ba2a68a159da
gurpreetjanjua <gurpreetsinghjanjua94@gmail.com>
2020-03-19 12:26:16 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Laravel + React + Redux
+Please follow the guide.
+
+### Follow steps to run the project
+
+1. `git clone`
+2. `create a .env file copy content from .env.example and update the values`
+3. `composer install && composer update`
+4. `npm install && npm run dev`
+5. `php artisan key:gen`
+6. `php artisan serve`
+
---
+++ resources/js/app.js
@@ -0,0 +1,43 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes React and other helpers. It's a great starting point while
+ * building robust, powerful web applications using React + Laravel.
+ */
+
+require('./bootstrap');
+
+/**
+ * Next, we will create a fresh React component instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+// require('./components/Example');
+import React from 'react'
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import Home from './components/Example';
+import About from './components/About';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
+const App = () => (
+    <>
+        <Router>
+            <Switch>
+                <Route path="/" component={Home} exact />
+                <Route path="/about" component={About} exact />
+            </Switch>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+            </ul>
+        </Router>
+    </>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
